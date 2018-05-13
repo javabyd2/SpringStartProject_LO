27b7e89fad9aba8e6894bba15c04e5119727ee3c
lukaszogn <lukaszogan@mat.umk.pl>
2018-05-13 09:56:32 +0200

    add service

--- src/main/java/com/sda/spring/demo/controller/Controller.java
+++ src/main/java/com/sda/spring/demo/controller/Controller.java
@@ -1,9 +1,12 @@
 package com.sda.spring.demo.controller;
 
 import com.sda.spring.demo.model.Book;
+import com.sda.spring.demo.model.BookAuthor;
 import com.sda.spring.demo.model.BookCategory;
 import com.sda.spring.demo.repository.BookRepository;
+import com.sda.spring.demo.service.AuthorService;
 import com.sda.spring.demo.service.BookService;
+import com.sda.spring.demo.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +19,12 @@ public class Controller {
 
     @Autowired
     private BookService bookService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private AuthorService authorService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String hello(){
@@ -34,6 +43,11 @@ public class Controller {
 
     @RequestMapping(value = "/categories", method = RequestMethod.GET)
     public List<BookCategory> showCategories(){
-        return categoryService.getCategories();
+        return categoryService.bookCategories();
+    }
+
+    @RequestMapping(value = "/authors", method = RequestMethod.GET)
+    public List<BookAuthor> showAuthors(){
+        return authorService.getAuthors();
     }
 }
