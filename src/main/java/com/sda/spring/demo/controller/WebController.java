715049f409ca3d5f70d76276ff95cc849159617b
lukaszogn <lukaszogan@mat.umk.pl>
2018-05-26 14:53:50 +0200

    dto

--- src/main/java/com/sda/spring/demo/controller/WebController.java
+++ src/main/java/com/sda/spring/demo/controller/WebController.java
@@ -1,10 +1,15 @@
 package com.sda.spring.demo.controller;
 
+import com.sda.spring.demo.model.Role;
 import com.sda.spring.demo.service.AuthorService;
+import com.sda.spring.demo.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class WebController {
@@ -31,6 +36,30 @@ public class WebController {
                         authorService.getAuthors()
                         );
         modelAndView.setViewName("authors");
+
+        return modelAndView;
+    }
+
+    @Autowired
+    private RoleService roleService;
+
+    @PostMapping(value = "/web/addrole")
+    public String addRole(@ModelAttribute("role")Role role,
+                          RedirectAttributes redirectAttributes
+                          ){
+
+        roleService.save(role);
+        redirectAttributes.addFlashAttribute("message",
+                "Dodano pomyślnie");
+
+        return "redirect:/web/roleform";
+    }
+
+    @GetMapping(value = "/web/roleform")
+    public ModelAndView roleForm(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("roleform");
+        modelAndView.addObject("role", new Role());
 
         return modelAndView;
     }
