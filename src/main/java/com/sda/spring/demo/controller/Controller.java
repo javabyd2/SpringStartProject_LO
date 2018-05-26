7fb36c291534e8832c7eae71088a29f4b15e5875
lukaszogn <lukaszogan@mat.umk.pl>
2018-05-26 10:50:09 +0200

    dto

--- src/main/java/com/sda/spring/demo/controller/Controller.java
+++ src/main/java/com/sda/spring/demo/controller/Controller.java
@@ -4,6 +4,7 @@ import com.sda.spring.demo.dto.UserPropDTO;
 import com.sda.spring.demo.model.Book;
 import com.sda.spring.demo.model.BookAuthor;
 import com.sda.spring.demo.model.BookCategory;
+import com.sda.spring.demo.model.User;
 import com.sda.spring.demo.repository.BookRepository;
 import com.sda.spring.demo.service.AuthorService;
 import com.sda.spring.demo.service.BookService;
@@ -42,10 +43,6 @@ public class Controller {
         return bookService.getBooks();
     }
 
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public List<UserPropDTO> showUserProp(){
-        return userService.getUsers();
-    }
 
     @RequestMapping(value = "/category", method = RequestMethod.POST)
     public BookCategory addCategory(
@@ -59,6 +56,12 @@ public class Controller {
             @RequestBody Book book
     ){
         return bookService.save(book);
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public List<UserPropDTO> showUsers(){
+
+        return userService.getUsers();
     }
 
     @RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
